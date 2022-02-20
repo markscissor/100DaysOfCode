88ef32a920fd56b164abd81a954031191fd57989
markscissor <91406286+markscissor@users.noreply.github.com>
2022-02-20 23:41:38 +0400

    Update and rename Day013_DebuggingExercises to Day013_Debugging_Exercises/main.py

--- Day013_Debugging_Exercises/main.py
+++ Day013_Debugging_Exercises/main.py
@@ -27,11 +27,11 @@
 # EXERCISE 3
 
 for number in range(1, 101):
-#   if number % 3 == 0 and number % 5 == 0:
-#     print("FizzBuzz")
-#   elif number % 3 == 0:
-#     print("Fizz")
-#   elif number % 5 == 0:
-#     print("Buzz")
-#   else:
-#     print(number)
+   if number % 3 == 0 and number % 5 == 0:
+     print("FizzBuzz")
+   elif number % 3 == 0:
+     print("Fizz")
+   elif number % 5 == 0:
+     print("Buzz")
+   else:
+     print(number)
