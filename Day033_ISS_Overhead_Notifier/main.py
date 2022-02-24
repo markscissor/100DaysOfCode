32e95591c0361a737479b1c7a0c115314c572493
markscissor <91406286+markscissor@users.noreply.github.com>
2022-02-24 21:45:24 +0400

    Add files via upload

---
+++ Day033_ISS_Overhead_Notifier/main.py
@@ -0,0 +1,60 @@
+import requests
+from datetime import datetime
+import smtplib
+import pytz
+import time
+import secret_codes
+
+
+# print(local_time)
+MY_EMAIL = secret_codes.MY_EMAIL
+MY_PASSWORD = secret_codes.MY_PASSWORD
+MY_LAT = 25.204849  # Your latitude
+MY_LONG = 55.270782  # Your longitude
+
+
+def check_iss():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
+
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
+    print("ISS Location: ", iss_latitude, iss_longitude)
+
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LONG,
+        "formatted": 0,
+    }
+
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+    # print(sunrise, sunset)
+    # time_now = datetime.now()
+    local_time = datetime.now(pytz.timezone('Asia/Dubai'))
+    hour_now = local_time.hour
+    # print(hour_now)
+
+    cond_1 = hour_now <= sunrise or hour_now >= sunset
+    cond_2 = abs(MY_LAT - iss_latitude) <= 5
+    cond_3 = abs(MY_LONG - iss_longitude) <= 5
+
+    if cond_1 and cond_2 and cond_3:
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+            connection.sendmail(
+                from_addr=MY_EMAIL,
+                to_addrs="mcnmunoz@gmail.com",
+                msg=f"Subject:Look up👆\n\nThe ISS is above you in the sky."
+            )
+            print("Email sent.")
+
+
+while True:
+    check_iss()
+    time.sleep(10)
---
+++ Day033_ISS_Overhead_Notifier/secret_codes.py
@@ -0,0 +1,2 @@
+MY_EMAIL = "your_email@gmail.com"
+MY_PASSWORD = "your_password"
