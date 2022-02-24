3f3cb7a548eed9a00c8ec8afdee85677a54c2abe
markscissor <91406286+markscissor@users.noreply.github.com>
2022-02-24 12:51:30 +0400

    Update main.py

--- Day032_Birthday_Wisher/main.py
+++ Day032_Birthday_Wisher/main.py
@@ -3,15 +3,9 @@ import pandas as pd
 import random
 import smtplib
 
-MY_EMAIL = "corroro001@gmail.com"
-MY_PASSWORD = "pass1324"
+MY_EMAIL = "USER@gmail.com"
+MY_PASSWORD = "PASSWORD"
 
-
-# 1. Update the birthdays.csv
-# 2. Check if today matches a birthday in the birthdays.csv
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual
-# name from birthdays.csv
-# 4. Send the letter generated in step 3 to that person's email address.
 
 data = pd.read_csv("birthdays.csv")
 birthday_list = data.to_dict(orient='records')
@@ -19,11 +13,7 @@ birthday_list = data.to_dict(orient='records')
 now = dt.datetime.now()
 month = now.month
 day = now.day
-# print(month, day)
 
-# Manual input of date
-month = 2
-day = 17
 email_list = []
 for person in birthday_list:
     if person['month'] == month and person['day'] == day:
@@ -35,8 +25,9 @@ for person in email_list:
 
     with open(letter_file, "r") as temp:
         msg = temp.readlines()
-
-    msg[0] = f'Dear {person["name"]},\n'
+    dear_msg = msg[0].split()
+    dear_msg[1] = f"{person['name']},\n"
+    msg[0] = " ".join(dear_msg)
     email_body = "".join(msg)
 
     with smtplib.SMTP("smtp.gmail.com") as connection:
@@ -44,7 +35,7 @@ for person in email_list:
         connection.login(user=MY_EMAIL, password=MY_PASSWORD)
         connection.sendmail(
             from_addr=MY_EMAIL,
-            to_addrs="corroro_001@yahoo.com",
+            to_addrs=person["email"],
             msg=f"Subject:Happy Birthday {person['name']}\n\n{email_body}"
         )
         print("Email sent.")
